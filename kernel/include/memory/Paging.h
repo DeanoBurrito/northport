a295eca2889dce7a9a543aee839b23f7dc77b464
Dean <dean243@hotmail.com>
2021-11-28 21:38:47 +1100

    Implemented paging.

---
+++ kernel/include/memory/Paging.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <NativePtr.h>
+#include <Platform.h>
+
+namespace Kernel::Memory
+{   
+    enum class PagingSize : size_t
+    {
+        //NOTE: is it illegal to try to allocate pages of this size, this is for returns only
+        NoSize = 0,
+
+        Physical = PAGE_FRAME_SIZE,
+
+        _4KB = 0x1000,
+        _2MB = 0x200000,
+        _1GB = 0x40000000,
+    };
+
+    enum class MemoryMapFlag : size_t
+    {
+        None = 0,
+        AllowWrites = 1 << 0,
+        AllowExecute = 1 << 1,
+        UserAccessible = 1 << 2,
+    };
+
+    FORCE_INLINE MemoryMapFlag operator&(const MemoryMapFlag& a, const MemoryMapFlag& b)
+    { return static_cast<MemoryMapFlag>((uint64_t)a & (uint64_t)b);}
+
+    FORCE_INLINE MemoryMapFlag operator|(const MemoryMapFlag& a, const MemoryMapFlag& b)
+    { return static_cast<MemoryMapFlag>((uint64_t)a | (uint64_t)b);}
+    
+    class PageTableManager
+    {
+    private:
+        static bool usingExtendedPaging;
+        sl::NativePtr topLevelAddress;
+        char lock;
+
+        FORCE_INLINE
+        void InvalidatePage(sl::NativePtr virtualAddr) const;
+        
+    public:
+        static PageTableManager* Local();
+
+        static void Setup();
+        void Init();
+        //set the higher half of this page table to the same entries as tla. 
+        void SetKernelPageRefs(sl::NativePtr tla);
+
+        void MakeActive() const;
+        bool IsActive() const;
+        bool PageSizeAvailable(PagingSize size) const;
+
+        //allocates a physical page and maps it at the specified address.
+        void MapMemory(sl::NativePtr virtAddr, MemoryMapFlag flags);
+        //maps the supplied physical address to the virtual address.
+        void MapMemory(sl::NativePtr virtAddr, sl::NativePtr physAddr, MemoryMapFlag flags);
+        //maps a region of memory of pageSize at the address supplied. Check which sizes are available before using this.
+        void MapMemory(sl::NativePtr virtAddr, PagingSize pageSize, MemoryMapFlag flags);
+        void MapMemory(sl::NativePtr virtAddr, sl::NativePtr physAddr, PagingSize pageSize, MemoryMapFlag flags);
+
+        //TODO: implement AllocPages()/FreePages() and MapRange()/UnmapRange()
+        // void MapRange(sl::NativePtr virtAddrBase, size_t count, MemoryMapFlag flags);
+        // void MapRange(sl::NativePtr virtAddrBase, sl::NativePtr physAddrBase, size_t count, MemoryMapFlag flags);
+        // void MapRange(sl::NativePtr virtAddrBase, size_t count, PagingSize pageSize, MemoryMapFlag flags);
+        // void MapRange(sl::NativePtr virtAddrBase, sl::NativePtr physAddrBase, size_t count, PagingSize pageSize, MemoryMapFlag flags);
+
+        //removes a mapping froma virtual region, returns the size of attached page.
+        PagingSize UnmapMemory(sl::NativePtr virtAddr);
+
+        //PagingSize UnmapRange(sl::NativePtr virtAddrBase, size_t count) const;
+    };
+}
---
+++ kernel/memory/Paging.cpp
@@ -0,0 +1,405 @@
+#include <memory/PhysicalMemory.h>
+#include <memory/Paging.h>
+#include <Memory.h>
+#include <Platform.h>
+#include <Cpu.h>
+#include <Log.h>
+#include <Maths.h>
+
+namespace Kernel::Memory
+{   
+    enum PageEntryFlag : uint64_t
+    {
+        None = 0,
+        
+        //set to make this entry used in translation
+        Present = 1 << 0,
+        //set if memory covered by this entry is read/write. Clear for read-only
+        RegionWritesAllowed = 1 << 1,
+        //set if usermode accesses are allowed through this entry. Cleared for only non-usermode accesses.
+        UserAccessAllowed = 1 << 2,
+        //MTRR stuff, cleared by default
+        CacheWriteThrough = 1 << 3,
+        //MTRR stuff, cleared by default
+        CacheDisable = 1 << 4,
+        //set if this entry has been used in a translation since last clear
+        Accessed = 1 << 5,
+        //reserved if PageSize is not set. Set when a write has occured through this entry.
+        Dirty = 1 << 6,
+        //tells translation to end at this level, and use the remaining address as the physical page.
+        //set this at level 2 for 2MB pages, level 3 for 1GB pages. (check for support first)
+        PageSize = 1 << 7,
+        //if enabled in EFER, makes this translation global
+        IsGlobal = 1 << 8,
+
+        //these are ignored by the cpu across all entries, and are available to use.
+        Custom0 = 1 << 9,
+        Custom1 = 1 << 10,
+        Custom2 = 1 << 11,
+
+        //reserved if PageSize is not set. MTRR stuff, cleared by default.
+        PAT = 1 << 12,
+        //if enabled in EFER, disabled instruction fetches through this entry
+        ExecuteDisable = 1ul << 63,
+    };
+
+    FORCE_INLINE PageEntryFlag operator |(PageEntryFlag a, PageEntryFlag b)
+    { return static_cast<PageEntryFlag>((uint64_t)a | (uint64_t)b); }
+
+    FORCE_INLINE PageEntryFlag operator ~(PageEntryFlag a)
+    { return static_cast<PageEntryFlag>(~(uint64_t)a); }
+    
+    struct PageTableEntry
+    {
+        uint64_t raw;
+
+        PageTableEntry(uint64_t data) : raw(data)
+        {}
+
+        void SetFlag(PageEntryFlag flagMask)
+        {
+            raw |= flagMask & 0xFFF0'0000'0000'0FFF;
+        }
+
+        void ClearFlag(PageEntryFlag flagMask)
+        {
+            raw &= ~flagMask & 0xFFF0'0000'0000'0FFF;
+        }
+
+        bool HasFlag(PageEntryFlag flag)
+        {
+            return (raw & (flag & 0xFFF0'0000'0000'0FFF)) != 0;
+        }
+
+        void SetAddr(sl::NativePtr address)
+        {
+            //zero address bits in raw entry, then move address to offset, and preserve control bits.
+            raw &= 0xFFF0'0000'0000'0FFF;
+            raw |= address.raw & ~(0xFFF0'0000'0000'0FFF);
+        }
+
+        sl::NativePtr GetAddr()
+        {
+            return raw & ~(0xFFF0'0000'0000'0FFF);
+        }
+    };
+
+    struct PageTable
+    {
+        PageTableEntry entries[512];
+    };
+
+    FORCE_INLINE void GetPageMapIndices(sl::NativePtr virtAddr, uint64_t* pml5Index, uint64_t* pml4Index, uint64_t* pml3Index, uint64_t* pml2Index, uint64_t* pml1Index)
+    {
+        //this works for all levels of paging, since the unused address bits are required to be zero (and ignored).
+        //This of course breaks with 32bit paging (it may work with PAE paging, not tested and not interested).
+        *pml5Index = (virtAddr.raw >> 48) & 0x1FF;
+        *pml4Index = (virtAddr.raw >> 39) & 0x1FF;
+        *pml3Index = (virtAddr.raw >> 30) & 0x1FF;
+        *pml2Index = (virtAddr.raw >> 21) & 0x1FF;
+        *pml1Index = (virtAddr.raw >> 12) & 0x1FF;
+    }
+
+    FORCE_INLINE
+    void PageTableManager::InvalidatePage(sl::NativePtr virtualAddress) const
+    {
+        asm volatile("invlpg %0" :: "m"(virtualAddress.ptr));
+    }
+
+    PageTableManager defaultPageTableManager;
+    PageTableManager* PageTableManager::Local()
+    {
+        return &defaultPageTableManager;
+    }
+
+    bool PageTableManager::usingExtendedPaging;
+    
+    void PageTableManager::Setup()
+    {
+        Log("Beginning system paging setup for pre-scheduler kernel.", LogSeverity::Info);
+
+        if (CPU::FeatureSupported(CpuFeature::ExecuteDisable))
+        {
+            //ensure EFER.NX is enabled (might already be enabled from bootloader)
+            uint64_t eferCurrent = CPU::ReadMsr(MSR_IA32_EFER);
+            eferCurrent |= (1 << 11);
+            CPU::WriteMsr(MSR_IA32_EFER, eferCurrent);
+
+            Log("Execute disable is available, and enabled in EFER.", LogSeverity::Verbose);
+        }
+        else
+            Log("Execute disable not available.", LogSeverity::Verbose);
+
+        if (CPU::FeatureSupported(CpuFeature::GigabytePages))
+            Log("Gigabyte pages are available.", LogSeverity::Verbose);
+
+        uint64_t cr4 = ReadCR4();
+        if ((cr4 & (1 << 12)) != 0)
+        {
+            usingExtendedPaging = true;
+            Log("System is setup for 5-level paging.", LogSeverity::Verbose);
+        }
+        else
+        {
+            usingExtendedPaging = false;
+            Log("System is setup for 4-level paging.", LogSeverity::Verbose);
+        }
+
+        Log("Paging setup successful.", LogSeverity::Info);
+    }
+
+    void PageTableManager::Init()
+    {
+        //topLevelAddress = PMM::Global()->AllocPage();
+        //sl::memset(topLevelAddress.ptr, 0, sizeof(PageTable));
+
+        topLevelAddress = ReadCR3();
+
+        Log("New page table initialized", LogSeverity::Info);
+    }
+
+    void PageTableManager::SetKernelPageRefs(sl::NativePtr tla)
+    {   
+        SpinlockAcquire(&lock);
+        
+        PageTable* foreignTable = tla.As<PageTable>();
+        PageTable* localTable = topLevelAddress.As<PageTable>();
+        for (size_t i = 0x100; i < 0x200; i++)
+        {
+            localTable->entries[i] = foreignTable->entries[i];
+        }
+
+        SpinlockRelease(&lock);
+    }
+
+    void PageTableManager::MakeActive() const
+    {
+        WriteCR3(topLevelAddress.raw);
+    }
+
+    bool PageTableManager::IsActive() const
+    {
+        return ReadCR3() == topLevelAddress.raw;
+    }
+
+    bool PageTableManager::PageSizeAvailable(PagingSize size) const
+    {
+        //this is pretty ugly I know, but it allows us to have 'Physical' be the same as one of the pre-determined sizes.
+        switch (size)
+        {
+        case PagingSize::Physical:
+            return true; //always available
+#if PAGE_FRAME_SIZE != 0x1000
+        case PagingSize::_4KB:
+            return true;
+#endif
+#if PAGE_FRAME_SIZE != 0x200000
+        case PagingSize::_2MB:
+            return true;
+#endif
+#if PAGE_FRAME_SIZE != 0x40000000
+        case PagingSize::_1GB:
+            return CPU::FeatureSupported(CpuFeature::GigabytePages);
+#endif
+
+        default:
+            return false;
+        }
+    }
+
+    void PageTableManager::MapMemory(sl::NativePtr virtAddr, MemoryMapFlag flags)
+    {
+        sl::NativePtr physAddr = PMM::Global()->AllocPage();
+        if (physAddr.ptr == nullptr)
+            Log("Could not allocate enough physical memory to map!", LogSeverity::Fatal);
+
+        MapMemory(virtAddr, physAddr, PagingSize::Physical, flags);
+        InvalidatePage(virtAddr);
+    }
+
+    void PageTableManager::MapMemory(sl::NativePtr virtAddr, sl::NativePtr physAddr, MemoryMapFlag flags)
+    {
+        MapMemory(virtAddr, physAddr, PagingSize::Physical, flags);
+        InvalidatePage(virtAddr);
+    }
+
+    void PageTableManager::MapMemory(sl::NativePtr virtAddr, PagingSize pageSize, MemoryMapFlag flags)
+    {
+        if (!PageSizeAvailable(pageSize))
+        {
+            Log("Cannot map memory, that paging size is not supported on this system.", LogSeverity::Error);
+            return;
+        }
+
+        const size_t nativePagesRequired = (size_t)pageSize / (size_t)PagingSize::Physical;
+        sl::NativePtr physAddr = PMM::Global()->AllocPages(nativePagesRequired);
+        if (physAddr.ptr == nullptr)
+            Log("Could not allocate enough physical memory to map!", LogSeverity::Fatal);
+        
+        //emit an error and then trash the tlb cache in protest.
+        Log("Non-4K paging is not supported yet.", LogSeverity::Error);
+        InvalidatePage(virtAddr);
+    }
+
+    void PageTableManager::MapMemory(sl::NativePtr virtAddr, sl::NativePtr physAddr, PagingSize size, MemoryMapFlag flags)
+    {
+        if (size == PagingSize::NoSize)
+        {
+            Log("Tried to map memory with no page size, ignoring.", LogSeverity::Warning);
+            return;
+        }
+
+        ScopedSpinlock scopeLock(&lock);
+        
+        PageEntryFlag templateFlags = PageEntryFlag::Present;
+        if ((uint64_t)(flags & MemoryMapFlag::AllowWrites) != 0)
+            templateFlags = templateFlags | PageEntryFlag::RegionWritesAllowed;
+        if ((uint64_t)(flags & MemoryMapFlag::UserAccessible) != 0)
+            templateFlags = templateFlags | PageEntryFlag::UserAccessAllowed;
+        if (CPU::FeatureSupported(CpuFeature::ExecuteDisable) && (uint64_t)(flags & MemoryMapFlag::AllowExecute) == 0)
+            templateFlags = templateFlags | PageEntryFlag::ExecuteDisable;
+
+        uint64_t pml5Index, pml4Index, pml3Index, pml2Index, pml1Index;
+        GetPageMapIndices(virtAddr, &pml5Index, &pml4Index, &pml3Index, &pml2Index, &pml1Index);
+
+        //TODO: smarter way of handling flags. What if a level3 page is marked nx, but we want to add an exec page beneath it?
+
+        PageTable* pageTable = topLevelAddress.As<PageTable>();
+        PageTableEntry* entry;
+        if (usingExtendedPaging)
+        {
+            //handle pml5 table
+            entry = &pageTable->entries[pml5Index];
+            
+            if (!entry->HasFlag(PageEntryFlag::Present))
+            {
+                entry->SetAddr(PMM::Global()->AllocPage());
+                sl::memset(entry->GetAddr().ptr, 0, PAGE_FRAME_SIZE);
+                entry->SetFlag(templateFlags);
+            }
+
+            pageTable = entry->GetAddr().As<PageTable>();
+        }
+
+        //handle pml4
+        entry = &pageTable->entries[pml4Index];
+        if (!entry->HasFlag(PageEntryFlag::Present))
+        {
+            entry->SetAddr(PMM::Global()->AllocPage());
+            sl::memset(entry->GetAddr().ptr, 0, PAGE_FRAME_SIZE);
+            entry->SetFlag(templateFlags);
+        }
+        pageTable = entry->GetAddr().As<PageTable>();
+
+        //for 1GB pages - early exit
+        entry = &pageTable->entries[pml3Index];
+        if (size == PagingSize::_1GB && CPU::FeatureSupported(CpuFeature::GigabytePages))
+        {
+            entry->SetFlag(templateFlags);
+            entry->SetFlag(PageEntryFlag::PageSize);
+            entry->SetAddr(physAddr);
+            return;
+        }
+
+        //handle pml3
+        if (!entry->HasFlag(PageEntryFlag::Present))
+        {
+            entry->SetAddr(PMM::Global()->AllocPage());
+            sl::memset(entry->GetAddr().ptr, 0, PAGE_FRAME_SIZE);
+            entry->SetFlag(templateFlags);
+        }
+        pageTable = entry->GetAddr().As<PageTable>();
+
+        //for 2MB pages - early exit
+        entry = &pageTable->entries[pml2Index];
+        if (size == PagingSize::_2MB)
+        {
+            entry->SetFlag(templateFlags);
+            entry->SetFlag(PageEntryFlag::PageSize); //tell cpu to end it's search here
+            entry->SetAddr(physAddr);
+            return;
+        }
+
+        //handle pml2
+        if (!entry->HasFlag(PageEntryFlag::Present))
+        {
+            entry->SetAddr(PMM::Global()->AllocPage());
+            sl::memset(entry->GetAddr().ptr, 0, PAGE_FRAME_SIZE);
+            entry->SetFlag(templateFlags);
+        }
+        pageTable = entry->GetAddr().As<PageTable>();
+
+        //handle pml1
+        entry = &pageTable->entries[pml1Index];
+        entry->SetFlag(templateFlags);
+        entry->SetAddr(physAddr);
+
+        InvalidatePage(physAddr);
+    }
+
+    PagingSize PageTableManager::UnmapMemory(sl::NativePtr virtAddr)
+    {
+        uint64_t pml5Index, pml4Index, pml3Index, pml2Index, pml1Index;
+        GetPageMapIndices(virtAddr, &pml5Index, &pml4Index, &pml3Index, &pml2Index, &pml1Index);
+
+        ScopedSpinlock scopeLock(&lock);
+
+        PageTable* pageTable = topLevelAddress.As<PageTable>();
+        PageTableEntry* entry;
+        if (usingExtendedPaging)
+        {
+            //pml5
+            entry = &pageTable->entries[pml5Index];
+            if (!entry->HasFlag(PageEntryFlag::Present))
+                return PagingSize::NoSize; //page not accessible, about
+            pageTable = entry->GetAddr().As<PageTable>();
+        }
+
+        //pml4
+        entry = &pageTable->entries[pml4Index];
+        if (!entry->HasFlag(PageEntryFlag::Present))
+            return PagingSize::NoSize;
+        pageTable = entry->GetAddr().As<PageTable>();
+
+        //pml3 and 1gb pages
+        entry = &pageTable->entries[pml3Index];
+        if (!entry->HasFlag(PageEntryFlag::Present))
+            return PagingSize::NoSize;
+        if (entry->HasFlag(PageEntryFlag::PageSize))
+        {
+            //it's a 1gb page
+            PMM::Global()->FreePages(entry->GetAddr().ptr, (uint64_t)PagingSize::_1GB / (uint64_t)PagingSize::Physical);
+            entry->SetAddr(0ul);
+            entry->ClearFlag(~PageEntryFlag::None);
+            return PagingSize::_1GB;
+        }
+        else
+            pageTable = entry->GetAddr().As<PageTable>();
+
+        //pml2 and 2mb pages
+        entry = &pageTable->entries[pml3Index];
+        if (!entry->HasFlag(PageEntryFlag::Present))
+            return PagingSize::NoSize;
+        if (entry->HasFlag(PageEntryFlag::PageSize))
+        {
+            PMM::Global()->FreePages(entry->GetAddr().ptr, (uint64_t)PagingSize::_2MB / (uint64_t)PagingSize::Physical);
+            entry->SetAddr(0ul);
+            entry->ClearFlag(~PageEntryFlag::None);
+            return PagingSize::_2MB;
+        }
+        else
+            pageTable = entry->GetAddr().As<PageTable>();
+
+        //pml1
+        entry = &pageTable->entries[pml1Index];
+        if (!entry->HasFlag(PageEntryFlag::Present))
+            return PagingSize::NoSize;
+        PMM::Global()->FreePage(entry->GetAddr().ptr);
+        entry->SetAddr(0ul);
+        entry->ClearFlag(~PageEntryFlag::None);
+        
+        //unmap memory
+        InvalidatePage(virtAddr);
+        return PagingSize::Physical;
+    }
+}
