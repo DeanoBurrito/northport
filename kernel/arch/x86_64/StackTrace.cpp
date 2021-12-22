3a4edccefbf230dee6f93927b0c647417828ab61
Ivan Gualandri <dreamos82@yahoo.it>
2021-12-22 14:42:39 +0000

    add stacktrace items to Vector

--- kernel/arch/x86_64/StackTrace.cpp
+++ kernel/arch/x86_64/StackTrace.cpp
@@ -1,5 +1,6 @@
 #include <StackTrace.h>
 #include <Log.h>
+#include <containers/Vector.h>
 
 namespace Kernel
 {
@@ -7,10 +8,13 @@ namespace Kernel
     {
         struct stackframe* stackItem;
         asm ("mov %%rbp,%0" : "=r"(stackItem) ::);
+        sl::Vector<NativeUInt> vec;
         while(stackItem != 0){
             Logf("GetStackTrace: %x\n", LogSeverity::Error, stackItem->eip);
+            vec.PushBack(stackItem->eip);
             stackItem = stackItem->ebp;
         }
+        Logf("GetStackTrace vec size: %x\n", LogSeverity::Error, vec.Size());
     }
 
 }
