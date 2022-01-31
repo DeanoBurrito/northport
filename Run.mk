7ddd6b9119541f8ec3709447be72e6ac5bdd1dfd
Dean <dean243@hotmail.com>
2022-01-31 19:10:32 +1100

    Use kvm when testing in vm, but not debugging.

--- Run.mk
+++ Run.mk
@@ -2,6 +2,7 @@
 
 QEMU_BIN = qemu-system-$(CPU_ARCH)
 QEMU_FLAGS = -machine q35 -smp cores=4 -monitor stdio -m 256M -cdrom $(ISO_TARGET) -debugcon /dev/stdout
+QEMU_RUN_FLAGS = --enable-kvm
 QEMU_DEBUG_FLAGS = -s -S
 
 DBG_BIN = gdb
@@ -11,7 +12,7 @@ DBG_FLAGS = $(KERNEL_FULL_FILEPATH) -ex "target remote :1234"
 pre-run-target: all
 
 run: pre-run-target
-	@$(QEMU_BIN) $(QEMU_FLAGS)
+	@$(QEMU_BIN) $(QEMU_FLAGS) $(QEMU_RUN_FLAGS)
 
 debug: pre-run-target
 	@$(QEMU_BIN) $(QEMU_FLAGS) $(QEMU_DEBUG_FLAGS)
