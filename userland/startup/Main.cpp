627ff04fe5d1869e45035ad7e03a09df003aa832
Dean <dean243@hotmail.com>
2022-05-03 23:42:55 +1000

    Userspace code updted for new syscalls. fixes failing tests.

--- userland/startup/Main.cpp
+++ userland/startup/Main.cpp
@@ -4,30 +4,14 @@
 void Main()
 {
     using namespace np::Syscall;
-    sl::NativePtr bufferAddr;
-    auto maybeWindowListener = OpenIpcStream("WindowServer/Listener", IpcStreamFlags::UseSharedMemory, bufferAddr.raw);
-    while (!maybeWindowListener)
-        maybeWindowListener = OpenIpcStream("WindowServer/Listener", IpcStreamFlags::UseSharedMemory, bufferAddr.raw);
-    Log("Got handle to window server, trying to open window ...", LogLevel::Verbose);
-    
-    for (size_t i = 0; i < 3; i++)
+
+    uint64_t data[] = { 0, 100, 100, 100, 100, 0, 0xF, 0 };
+    for (size_t i = 0; i < 10; i++)
     {
-        //now we've successfully gotten a handle, try to create a window
-        sl::NativePtr request = bufferAddr.raw + 16;
-        sl::StackPush<uint64_t, true>(request, 200);
-        sl::StackPush<uint64_t, true>(request, 100);
-        sl::StackPush<uint64_t, true>(request, i * 200 + 50);
-        sl::StackPush<uint64_t, true>(request, i * 200 + 50);
-        sl::StackPush<uint64_t, true>(request, 0);
-        sl::StackPush<uint64_t, true>(request, 0);
-        sl::StackPush<uint64_t, true>(request, 'a');
-        sl::StackPush<uint64_t, true>(request, 0);
-
-        Log("Sending CreateWindowRequest", LogLevel::Verbose);
-        *bufferAddr.As<uint64_t>(8) = 0; //clear response byte so we dont accidentally read it ourselves
-        *bufferAddr.As<uint64_t>() = 1;
-
-        while (sl::MemRead<uint64_t>(bufferAddr.As<void>(8)) != 2);
+        data[3] = 100 + i * 64;
+        data[4] = 100 + i * 16;
+        while (!PostToMailbox("WindowServer/Incoming", {(void*)data, sizeof(data)}))
+            for (size_t j = 0; j < 10000; j++);
     }
 
     while (1);
