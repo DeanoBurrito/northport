82b763db51d67e20678d4061a532e2cd16ca2fa9
Dean <dean243@hotmail.com>
2022-06-01 14:24:10 +1000

    Added RetiringIdAllocator, closes #38

--- libs/np-syslib/include/IdAllocator.h
+++ libs/np-syslib/include/IdAllocator.h
@@ -43,5 +43,54 @@ namespace sl
         }
     };
 
+    template<typename BackingType, size_t RetiredCount>
+    class RetiringIdAllocator
+    {
+    private:
+        BackingType nextId;
+        sl::Vector<BackingType> freedGaps;
+        sl::Vector<BackingType> retiredQueue;
+        
+    public:
+        RetiringIdAllocator() : nextId(0)
+        {
+            retiredQueue.EnsureCapacity(RetiredCount);
+        }
+
+        void Reset()
+        {
+            freedGaps.Clear();
+            retiredQueue.Clear();
+            nextId = 0;
+        }
+
+        BackingType Alloc()
+        {
+            BackingType ret;
+            if (freedGaps.Size() > 0)
+                ret = freedGaps.PopBack();
+            else
+            {
+                ret = nextId;
+                nextId++;
+            }
+            return ret;
+        }
+
+        void Free(BackingType id)
+        {
+            if (retiredQueue.Size() == retiredQueue.Capacity())
+            {
+                const size_t recycledId = retiredQueue.PopBack(); //TODO: should implement a queue<T> and use PopFront() here
+                if (recycledId == nextId - 1)
+                    nextId;
+                else
+                    freedGaps.PushBack(id);
+            }
+
+            retiredQueue.PushBack(id);
+        }
+    };
+
     using UIdAllocator = IdAllocator<size_t>;
 }
