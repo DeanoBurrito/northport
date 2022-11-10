3e8c46a1557cd5afaa7896845f4385f18543e55b
Dean <dean243@hotmail.com>
2022-11-10 22:50:39 +1100

    Added VALIDATE() macro

--- kernel/include/debug/Log.h
+++ kernel/include/debug/Log.h
@@ -32,6 +32,15 @@ using Npk::Debug::Log;
         Log("Assert failed (%s:%u in %s): ASSERT_UNREACHABLE reached.", LogLevel::Fatal, __FILE__, __LINE__, __PRETTY_FUNCTION__); \
         __builtin_unreachable(); \
     }
+
+    #define VALIDATE(cond, retval, msg) \
+    { \
+        if (!(cond)) \
+        { \
+            Log("Validation failed (%s:%u in %s): %s", LogLevel::Error, __FILE__, __LINE__, __PRETTY_FUNCTION__,(msg)); \
+            return retval; \
+        } \
+    }
 #else
     #define ASSERT(cond, msg) \
     { \
@@ -43,5 +52,14 @@ using Npk::Debug::Log;
     { \
         Log("Assert failed (%s:%u): ASSERT_UNREACHABLE reached.", LogLevel::Fatal, __FILE__, __LINE__); \
         __builtin_unreachable(); \
+    }
+
+    #define VALIDATE(cond, retval, msg) \
+    { \
+        if (!(cond)) \
+        { \
+            Log("Check failed (%s:%u): %s", LogLevel::Error, __FILE__, __LINE__, (msg)); \
+            return retval; \
+        } \
     }
 #endif
