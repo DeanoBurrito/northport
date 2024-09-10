d992b724e9cf3335918af714e2fbd25e64fd8164
dean <dean243@hotmail.com>
2024-09-10 18:08:55 +1000

    kernel: explicitly specify linker symbol visibility

--- kernel/include/boot/LinkerSyms.h
+++ kernel/include/boot/LinkerSyms.h
@@ -2,12 +2,20 @@
 
 extern "C"
 {
+    [[gnu::visibility("default")]]
     extern char KERNEL_BLOB_BEGIN[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_BLOB_SIZE[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_TEXT_BEGIN[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_TEXT_SIZE[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_RODATA_BEGIN[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_RODATA_SIZE[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_DATA_BEGIN[];
+    [[gnu::visibility("default")]]
     extern char KERNEL_DATA_SIZE[];
 }
