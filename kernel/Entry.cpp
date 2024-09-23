f1a923117ff6e3c603aa86767b473292d4d6406e
dean <dean243@hotmail.com>
2024-09-23 22:23:51 +1000

    kernel: added EarlyVmAlloc hack

--- kernel/Entry.cpp
+++ kernel/Entry.cpp
@@ -3,33 +3,91 @@
 #include <core/Config.h>
 #include <core/Log.h>
 #include <core/Pmm.h>
+#include <core/Smp.h>
+#include <core/WiredHeap.h>
 #include <interfaces/intra/BakedConstants.h>
 #include <interfaces/intra/LinkerSymbols.h>
 #include <interfaces/loader/Generic.h>
 #include <services/AcpiTables.h>
+#include <services/MagicKeys.h>
+#include <Maths.h>
 
 namespace Npk
 {
     uintptr_t hhdmBase;
     size_t hhdmLength;
+    uintptr_t earlyVmBase;
+    bool earlyVmEnabled;
+
+    void* EarlyVmAlloc(uintptr_t paddr, size_t length, bool writable, bool mmio, const char* tag)
+    {
+        VALIDATE_(earlyVmEnabled, nullptr);
+        Log("EarlyVmAlloc: paddr=0x%tx, len=0x%zu, tag=%s%s%s", LogLevel::Verbose, paddr,
+            length, tag, writable ? ", writable" : "", mmio ? ", mmio" : "");
+
+        HatFlags flags = HatFlag::Global;
+        if (writable)
+            flags.Set(HatFlag::Write);
+        if (mmio)
+            flags.Set(HatFlag::Mmio);
+
+        const size_t granuleSize = HatGetLimits().modes[0].granularity;
+        for (size_t i = 0; i < length; i += granuleSize)
+            HatDoMap(KernelMap(), earlyVmBase + i, paddr + i, 0, flags, false);
+
+        const uintptr_t retAddr = earlyVmBase;
+        earlyVmBase += sl::AlignUp(length, granuleSize);
+        return reinterpret_cast<void*>(retAddr);
+    }
+
+    uintptr_t EarlyVmControl(bool enable)
+    {
+        earlyVmEnabled = enable;
+        if (enable)
+        {
+            earlyVmBase = hhdmBase + hhdmLength;
+            earlyVmBase += sl::AlignUp(hhdmLength / PageSize() * sizeof(Core::PageInfo), PageSize());
+        }
+
+        Log("EarlyVmControl: enabled=%s, base=0x%tx", LogLevel::Verbose, 
+            enable ? "yes" : "no", earlyVmBase);
+        return earlyVmBase;
+    }
 
     void InitThread(void*)
-    {}
+    { ASSERT_UNREACHABLE(); }
 
     void ReclaimLoaderMemoryThread(void*)
-    {}
+    { ASSERT_UNREACHABLE(); }
 
     void PerCoreEntry(size_t myId)
     {
-        //local pmm and heap caches
+        HatMakeActive(KernelMap(), true);
+        ArchInitCore(myId);
+
+        Core::InitLocalSmpMailbox();
+        //Core::InitLocalHeapCache();
+        //Core::Pmm::Global().InitLocalCache();
+        //TODO: intr routing, scheduler, local logging
+        //TODO: local timers
     }
 
     [[noreturn]]
     void ExitCoreInit()
-    { Halt(); }
+    { 
+        Log("core done", LogLevel::Debug);
+        Halt(); 
+    }
+
+    static void HandleMagicKeyPanic(npk_key_id key)
+    {
+        (void)key;
+        Log("Manually triggered by magic key.", LogLevel::Fatal);
+    }
 
     extern "C" void KernelEntry()
     {
+        earlyVmEnabled = false;
         ArchKernelEntry();
         Core::InitGlobalLogging();
 
@@ -51,22 +109,23 @@ namespace Npk
         HatInit();
         HatMakeActive(KernelMap(), true);
         Core::Pmm::Global().Init();
+        EarlyVmControl(true);
 
-        if (auto rsdp = GetRsdp(); rsdp.HasValue())
-            Services::SetRsdp(*rsdp);
+        //if (auto rsdp = GetRsdp(); rsdp.HasValue())
+            //Services::SetRsdp(*rsdp);
         //if (auto fdt = GetDtb(); fdt.HasValue())
             //Services::SetFdtPoitner(*fdt); TODO: import smoldtb and do dtb stuff
 
-        //ArchLateKernelEntry();
-        //Add early magic keys: P for panic
+        ArchLateKernelEntry();
         //Core::LateInitConfigStore();
-        //init early gterms
+        if (Core::GetConfigNumber("kernel.enable_panic_magic_key", false))
+            Services::AddMagicKey(npk_key_id_p, HandleMagicKeyPanic);
 
         //driver subsystem, threading and vfs init
-        //startup APs, global timers, system clock
+        //startup APs
+        //system uptime clock
 
-        Log("done for now", LogLevel::Debug);
-        while (true)
-        { asm(""); }
+        StartupAps();
+        ExitCoreInit();
     }
 }
--- kernel/include/Entry.h
+++ kernel/include/Entry.h
@@ -1,14 +1,31 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
 
 namespace Npk
 {
+    //A temporary hack for early in the kernel init sequence, it allows for VmAlloc()-like
+    //functionality before the virtual memory substem is available.
+    //Some devices require MMIO to be mapped (x86 apics for example) or there may be
+    //firmware features that arent required to be in the hhdm (example: acpi tables, fdt blob),
+    //this allows them to be mapped and used. Note that these mappings cannot be unmapped and
+    //are not managable by the VM subsystem once running. Use sparingly.
+    //The tag is added to the log output to help identify uses of this function
+    void* EarlyVmAlloc(uintptr_t paddr, size_t length, bool writable, bool mmio, const char* tag);
+
+    //Provides control over EarlyVmAlloc. By default this feature is disabled. It is also
+    //disabled once the VM subsystem is running. If `enable` is true, `newAllocBase` is used
+    //to determine where the allocator will start assigning addresses from.
+    //It returns the previous value of the highest allocated address.
+    uintptr_t EarlyVmControl(bool enable, uintptr_t newAllocBase);
+
     //started on the first core to finish its local init and begin scheduling.
     void InitThread(void*);
 
     //called by boot protocol AP entry code.
     void PerCoreEntry(size_t myId);
+
     //called by boot protocol AP entry code after PerCoreEntry(), and by BSP later on.
     [[noreturn]]
     void ExitCoreInit();
