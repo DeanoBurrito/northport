50b44f1e27f9e9045facb101e042552d5c274da2
dean <dean243@hotmail.com>
2024-12-23 19:54:48 +1100

    build: updated to use limine v8

--- misc/DevelCache.mk
+++ misc/DevelCache.mk
@@ -6,7 +6,7 @@ export LIMINE_DIR = $(DEVEL_CACHE_DIR)/limine
 ifeq ($(CPU_ARCH), x86_64)
 	OVMF_DOWNLOAD_URL = https://retrage.github.io/edk2-nightly/bin/RELEASEX64_OVMF.fd
 else ifeq ($(CPU_ARCH), riscv64)
-	OVMF_DOWNLOAD_URL = https://retrage.github.io/edk2-nightly/bin/RELEASERISCV64_VIRT.fd
+	OVMF_DOWNLOAD_URL = https://retrage.github.io/edk2-nightly/bin/RELEASERISCV64_VIRT_CODE.fd
 	OVMF_POST_CMD = && dd if=/dev/zero of=$(OVMF_FILE) bs=1 count=0 seek=33554432
 endif
 
@@ -26,10 +26,10 @@ $(DEVEL_CACHE_DIR)/exists/ovmf-$(CPU_ARCH): $(DEVEL_CACHE_DIR)
 $(DEVEL_CACHE_DIR)/exists/limine: $(DEVEL_CACHE_DIR)
 	$(LOUD)-rm -rf $(LIMINE_DIR)
 	$(LOUD)git clone https://github.com/limine-bootloader/limine.git \
-		--branch=v7.x-binary --depth 1 $(LIMINE_DIR)
+		--branch=v8.x-binary --depth 1 $(LIMINE_DIR)
 	$(LOUD)cd $(LIMINE_DIR); make all
 	$(LOUD)touch $@
-	@printf "$(C_YELLOW)[Cache]$(C_RST) Limine repo cloned from latest v5 release.\r\n"
+	@printf "$(C_YELLOW)[Cache]$(C_RST) Limine repo cloned from latest v8 release.\r\n"
 
 .PHONY: cache-clean
 cache-clean: $(DEVEL_CACHE_DIR)
