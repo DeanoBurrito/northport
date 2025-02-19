972849c7804ca9d48ca8269e603e51bd76554c78
dean <dean243@hotmail.com>
2025-02-19 20:03:35 +1100

    build: fix for generated kernel source files in unity build

--- kernel/Local.mk
+++ kernel/Local.mk
@@ -45,7 +45,7 @@ $(KERNEL_TARGET): $(KERNEL_OBJS) $(KERNEL_LD_SCRIPT)
 	@printf "$(C_BLUE)[Kernel]$(C_RST) $(C_GREEN)Done.$(C_RST)\n"
 
 .PHONY: $(UNITY_SOURCE_FILE)
-$(UNITY_SOURCE_FILE):
+$(UNITY_SOURCE_FILE): $(addprefix kernel/, $(KERNEL_CXX_SRCS))
 	@mkdir -p $(@D)
 	$(file > $(UNITY_SOURCE_FILE))
 	$(foreach S, $(KERNEL_CXX_SRCS), $(shell printf "#include <$S>\n" >> $(UNITY_SOURCE_FILE)))
