51502ab95b04b265ddb00b0a472434d66ba7435a
dean <dean243@hotmail.com>
2026-06-20 19:15:18 +1000

    kernel/vm: kernel stack allocator correctly adds stack size to returned pointer on success

--- kernel/vm/KernelStack.cpp
+++ kernel/vm/KernelStack.cpp
@@ -10,12 +10,20 @@ namespace Npk
         if (ptr == nullptr)
             return NpkStatus::Shortage;
 
+        auto addr = reinterpret_cast<uintptr_t>(ptr);
+        addr += KernelStackSize();
+        ptr = reinterpret_cast<void*>(addr);
+
         *stack = ptr;
         return NpkStatus::Success;
     }
 
     void FreeKernelStack(void* stack)
     {
+        auto addr = reinterpret_cast<uintptr_t>(stack);
+        addr -= KernelStackSize();
+        stack = reinterpret_cast<void*>(addr);
+
         auto succ = PoolFreeWired(stack, KernelStackSize(), KernelStackTag);
 
         NPK_ASSERT(succ);
